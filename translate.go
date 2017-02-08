8a084294cbf59cb598f308f4163549813645af7b
soft as HELL <softashell@cock.li>
2017-02-08 16:47:42 +0200

    Better use of gorequest

--- translate.go
+++ translate.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"encoding/json"
 	log "github.com/Sirupsen/logrus"
 	"github.com/parnurzeal/gorequest"
 	"time"
@@ -22,38 +21,28 @@ type translateResponse struct {
 
 func translateString(text string) (string, error) {
 	start := time.Now()
-	jsonStruct := translateRequest{
+
+	var response translateResponse
+
+	request := translateRequest{
 		From: "ja",
 		To:   "en",
 		Text: text,
 	}
 
-	// Convert json object to string
-	jsonString, err := json.Marshal(jsonStruct)
-	if err != nil {
-		log.Error("Failed to marshal JSON API request", err.Error())
-	}
-
-	// Post the request
-	resp, reply, errs := gorequest.New().Post("http://127.0.0.1:3000/api/translate").Send(string(jsonString)).EndBytes()
 	// TODO: Short timeout on connection failure, give up after few and cancel request if there's a new string to translate
+	resp, reply, errs := gorequest.New().Post("http://127.0.0.1:3000/api/translate").
+		Type("json").SendStruct(&request).EndStruct(&response)
 	for _, err := range errs {
 		log.WithFields(log.Fields{
 			"response": resp,
 			"reply":    reply,
-		}).Error(err.Error())
+		}).Error(err)
+
 		return "", err
 	}
 
-	var response translateResponse
-
-	if err := json.Unmarshal(reply, &response); err != nil {
-		log.Error("Failed to unmarshal JSON API response", err.Error())
-		return "", err
-	}
-
-	var out string
-	out = response.TranslationText
+	out := response.TranslationText
 
 	log.WithFields(log.Fields{
 		"time": time.Since(start),
